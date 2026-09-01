f2fc1b44bbd17c512da95b3e8304f463c29cb2ec
SeraphimeZelel <hajarki1229@gmail.com>
2026-09-01 18:04:11 +0700

    Yes

---
+++ .extension/Workspace Indicator/extension.js
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2011 Erick Pérez Castellanos <erick.red@gmail.com>
+// SPDX-FileCopyrightText: 2011 Giovanni Campagna <gcampagna@src.gnome.org>
+// SPDX-FileCopyrightText: 2017 Florian Müllner <fmuellner@gnome.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+import {WorkspaceIndicator} from './workspaceIndicator.js';
+
+export default class WorkspaceIndicatorExtension extends Extension {
+    enable() {
+        this._indicator = new WorkspaceIndicator({
+            extension: this,
+        });
+        Main.panel.addToStatusArea('workspace-indicator', this._indicator, 0, 'left');
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
+}
